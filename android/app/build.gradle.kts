631d49b8ecd72c4d56fe2eb32b2e354dbe7ef99e
visino2 <abavictor07@gmail.com>
2026-04-22 10:39:32 +0100

    chore: add release keystore, signing config, and ADI verification file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,11 +20,17 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "converf-release"
+            keyPassword = "converf123"
+            storeFile = file("converf-release.jks")
+            storePassword = "converf123"
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.converf.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
@@ -33,9 +39,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
